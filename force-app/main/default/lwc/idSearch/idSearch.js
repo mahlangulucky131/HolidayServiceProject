5a1694c1c60d261672b11ed9f64b75b79320b9f8
Sheriff <mahlangulucky131@gmail.com>
2024-08-23 18:55:03 +0200

    Removed Unrelated Pages and Controller

--- force-app/main/default/lwc/idSearch/idSearch.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, track } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import ID_Number__c from "@salesforce/schema/Id_Number_Search__c.ID_Number__c";
-
-export default class IdSearch extends LightningElement {
-    @track Id_Number_Search__c;
-    vfid = 'https://my-demo--c.eu11.visual.force.com/apex/MyVisualforcePage?Id=';
-
-    }
